fa6350059d1192603fe3fd80ce77671733024baf
WWW <wikran644@gmail.com>
2023-12-26 18:03:45 +0700

    Added randomness of buttons.

--- 15_puzzle_main.py
+++ 15_puzzle_main.py
@@ -1,17 +1,18 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
-
+from random import shuffle
 
 class FifteenPuzzle(GridLayout):
     def __init__(self, **kwargs):
         super(FifteenPuzzle, self).__init__(**kwargs)
         self.cols = 4
         self.spacing = 5
-
+        self.tiles = [str(i) for i in range(1, 16)] + ['']
+        shuffle(self.tiles)
         
-        for i in range(15):
-            button = Button(text = f'{i + 1}')
+        for tile in self.tiles:
+            button = Button(text=tile)
             self.add_widget(button)
 
 
