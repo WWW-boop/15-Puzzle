d966eab4a7ec819e8b20108887994bd9c4fffce2
WWW <wikran644@gmail.com>
2023-12-26 17:35:54 +0700

    Create a 2*2 table.

--- 15_puzzle_main.py
+++ 15_puzzle_main.py
@@ -1,10 +1,23 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
 
-   
-class FifteenPuzzleApp(App):
+
+class FifteenPuzzle(GridLayout):
+    def __init__(self, **kwargs):
+        super(FifteenPuzzle, self).__init__(**kwargs)
+        self.cols = 2  
+        
+        for i in range(4):
+            button = Button(text = f'{i + 1}')
+            self.add_widget(button)
+
+
+
+
+class FifteenPuzzleApp(App): 
     def build(self):
-        return GridLayout()
+        return FifteenPuzzle()
 
 if __name__ == '__main__':
     FifteenPuzzleApp().run()
