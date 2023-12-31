7c40cc049f1639b0d7f8bde1db08e39c583d90dc
WWW <119711067+WWW-boop@users.noreply.github.com>
2023-12-31 19:26:30 +0700

    Add smooth animations.

--- 15_puzzle_main.py
+++ 15_puzzle_main.py
@@ -6,6 +6,7 @@ from kivy.uix.label import Label
 
 from kivy.clock import Clock
 from kivy.graphics import Color, Rectangle
+from kivy.animation import Animation
 from random import shuffle
 
 class FifteenPuzzle(BoxLayout):
@@ -53,14 +54,36 @@ class FifteenPuzzle(BoxLayout):
         empty_index = self.tiles.index('')
         if self.is_adjacent(current_index, empty_index):
             self.tiles[current_index], self.tiles[empty_index] = self.tiles[empty_index], self.tiles[current_index]
-            self.update_buttons()
+            self.animate_tile_move(instance, current_index, empty_index)
 
             if self.is_solved():
                 self.game_running = False
                 Clock.unschedule(self.update_timer)
                 self.timer_label.text = "Congratulations! Puzzle Solved!"
 
+    def animate_tile_move(self, instance, from_index, to_index):
+        row_from, col_from = divmod(from_index, 4)
+        row_to, col_to = divmod(to_index, 4)
 
+        cell_size = instance.width, instance.height
+        target_pos = (
+            col_to * cell_size[0] + self.game_layout.x,
+            (3 - row_to) * cell_size[1] + self.game_layout.y,
+        )
+
+        anim = Animation(pos=target_pos, duration=0.2)
+        anim.start(instance)
+        anim.bind(on_complete=lambda _, __: self.on_animation_complete())
+
+    def on_animation_complete(self):
+        self.update_buttons()
+        if self.check_win():
+            Clock.unschedule(self.update_timer)
+            self.game_running = False
+
+    def check_win(self):
+        return self.tiles == [str(i) for i in range(1, 16)] + ['']
+    
     def is_adjacent(self, index1, index2):
         row1, col1 = divmod(index1, 4)
         row2, col2 = divmod(index2, 4)
